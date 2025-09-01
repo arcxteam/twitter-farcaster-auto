2c6ec83730b8b4e5d47c583e0a920ee152b55a62
0xgrey <33593974+arcxteam@users.noreply.github.com>
2025-09-01 21:24:48 +0700

    Update; support BlueSky tabs

--- background.js
+++ background.js
@@ -1,6 +1,8 @@
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   if (changeInfo.status === "complete" && 
-      (tab.url.includes("x.com/home") || tab.url.includes("farcaster.xyz"))) {
+      (tab.url.includes("x.com/home") || 
+       tab.url.includes("farcaster.xyz") ||
+       tab.url.includes("bsky.app"))) {
     chrome.storage.local.get("enabled", (data) => {
       if (data.enabled) {
         chrome.scripting.executeScript({
