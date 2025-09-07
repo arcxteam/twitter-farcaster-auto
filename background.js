01e0c9dee8c744fea1389574649cdbc57cb067c9
0xgrey <33593974+arcxteam@users.noreply.github.com>
2025-09-07 23:40:57 +0700

    Update v1.0.9 support Linkedin & Quora

--- background.js
+++ background.js
@@ -2,7 +2,10 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   if (changeInfo.status === "complete" && 
       (tab.url.includes("x.com/home") || 
        tab.url.includes("farcaster.xyz") ||
-       tab.url.includes("bsky.app"))) {
+       tab.url.includes("bsky.app") ||
+       tab.url.includes("linkedin.com/feed") ||
+       tab.url.includes("quora.com") ||
+       tab.url.includes("reddit.com"))) {
     chrome.storage.local.get("enabled", (data) => {
       if (data.enabled) {
         chrome.scripting.executeScript({
